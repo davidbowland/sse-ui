b20798698ae669d7cc9cfd4b468f90e0e399d9ed
David Bowland <dbowland1@gmail.com>
2025-04-09 03:48:00 -0500

    Attempt to fix two button component padding

--- src/components/claim-prompt/two-buttons.tsx
+++ src/components/claim-prompt/two-buttons.tsx
@@ -4,17 +4,19 @@ import React from 'react'
 export interface TwoButtonsProps {
   button1: React.ReactNode
   button2: React.ReactNode
+  hasExtraPadding?: boolean
 }
 
-const TwoButtons = ({ button1, button2 }: TwoButtonsProps): React.ReactNode => {
+const TwoButtons = ({ button1, button2, hasExtraPadding }: TwoButtonsProps): React.ReactNode => {
+  const padding = hasExtraPadding ? { paddingRight: { sm: 'initial', xs: 6 } } : {}
   return (
     <Grid container spacing={3}>
       <Grid item md={2} order={{ xs: 1 }} sm={1} xs={0}></Grid>
-      <Grid item md={3} order={{ sm: 2, xs: 4 }} sm={4} xs={12}>
+      <Grid item md={3} order={{ sm: 2, xs: 4 }} sm={4} sx={padding} xs={12}>
         {button1}
       </Grid>
       <Grid item order={{ xs: 3 }} sm={2} xs={0}></Grid>
-      <Grid item md={3} order={{ sm: 4, xs: 2 }} sm={4} xs={12}>
+      <Grid item md={3} order={{ sm: 4, xs: 2 }} sm={4} sx={padding} xs={12}>
         {button2}
       </Grid>
       <Grid item md={2} order={{ xs: 5 }} sm={1} xs={0}></Grid>
