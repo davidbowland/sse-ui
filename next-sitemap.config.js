630987535188f0549b25309b231722bf314e4c90
David Bowland <dbowland1@gmail.com>
2026-03-31 11:35:27 -0500

    fix: restore missing eslint dependencies and fix lint configuration

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://sse.dbowland.com',
   exclude: ['/c/__placeholder__'],
+  siteUrl: 'https://sse.dbowland.com',
 }
