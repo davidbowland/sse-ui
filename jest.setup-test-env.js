99e855f957d3527b7e84df3ceba974f6c8b8b879
David Bowland <dbowland1@gmail.com>
2026-03-31 00:28:48 -0500

    chore: migrate Jest config from Gatsby to Next.js

--- jest.setup-test-env.js
+++ jest.setup-test-env.js
@@ -1,9 +1,4 @@
-// Gatsby loader shim
-global.___loader = {
-  enqueue: jest.fn(),
-}
-
 // Environment variables
-process.env.GATSBY_SSE_API_BASE_URL = 'http://localhost'
+process.env.NEXT_PUBLIC_SSE_API_BASE_URL = 'http://localhost'
 
 window.URL.createObjectURL = jest.fn()
