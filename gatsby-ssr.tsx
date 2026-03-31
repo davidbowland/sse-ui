11034a84f42eb5441a39d8a99c7f0fbac37a66c1
David Bowland <dbowland1@gmail.com>
2026-03-31 00:28:52 -0500

    chore: remove Gatsby-specific files

--- gatsby-ssr.tsx
+++
@@ -1,8 +0,0 @@
-import type { GatsbyBrowser } from 'gatsby'
-import React from 'react'
-
-import Themed from '@components/themed'
-
-export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({ element }): JSX.Element => {
-  return <Themed>{element}</Themed>
-}
