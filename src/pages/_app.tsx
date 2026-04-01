b6460c46831af1adb0951825013567aeca73cddc
David Bowland <dbowland1@gmail.com>
2026-03-31 22:09:13 -0500

    feat: move dark mode and Disclaimer to _app, add flash prevention to _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,24 @@
+import '@fontsource/roboto'
 import type { AppProps } from 'next/app'
-import React from 'react'
+import React, { useEffect } from 'react'
 
-import Themed from '@components/themed'
+import '../styles/global.css'
+import Disclaimer from '@components/disclaimer'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const mq = window.matchMedia('(prefers-color-scheme: dark)')
+    const applyTheme = (dark: boolean) => document.documentElement.classList.toggle('dark', dark)
+    applyTheme(mq.matches)
+    const handler = (e: MediaQueryListEvent) => applyTheme(e.matches)
+    mq.addEventListener('change', handler)
+    return () => mq.removeEventListener('change', handler)
+  }, [])
+
   return (
-    <Themed>
+    <>
       <Component {...pageProps} />
-    </Themed>
+      <Disclaimer />
+    </>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: '(function(){try{var d=window.matchMedia(\'(prefers-color-scheme: dark)\').matches;if(d)document.documentElement.classList.add(\'dark\')}catch(e){}})()',
+          }}
+        />
         <Main />
         <NextScript />
       </body>
