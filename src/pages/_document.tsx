ecbcd067619e3235810c5c5ec0e45362b3084dd6
David Bowland <dbowland1@gmail.com>
2026-07-12 00:27:16 -0500

    Add app favicon and stop treating public/ as fully generated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+        <link href="/favicon-32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/favicon-16.png" rel="icon" sizes="16x16" type="image/png" />
+        <link href="/favicon.ico" rel="shortcut icon" />
+        <link href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+      </Head>
       <body>
         <script
           dangerouslySetInnerHTML={{
